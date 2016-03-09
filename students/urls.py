25e1cba5ed66af5fd4b5e1b402e1465e652f45ba
green <green@debian>
2016-03-09 22:19:56 +0200

    1/2of12.1

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^(?P<st_id>\d+)/', views.detail, name="detail"),
-    url(r'^', views.list_view, name="list_view"),
+    url(r'^edit/(?P<pk>\d+)/', views.edit, name="edit"),
+    url(r'^add/', views.create, name="add"),
+    url(r'^$', views.list_view, name="list_view"),
 )
--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from students.models import Student
+from students.forms import StudentModelForm
+from django.contrib import messages
 
 
 def detail(request, st_id):
@@ -16,3 +18,35 @@ def list_view(request):
     except IndexError:
         students = Student.objects.all()
         return render(request, 'students/list.html', {'students': students})
+
+
+def create(request):
+    form = StudentModelForm
+    if request.method == 'GET':
+        return render(request, 'students/add.html', {'form': form})
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            surname = form.cleaned_data['surname']
+            form.save()
+            messages.success(request, 'Student %s %s has been successfully added' % (name, surname))
+            return redirect('students:list_view')
+
+
+def edit(request, pk):
+    #if request.method == 'GET':
+    pk = int(pk)
+    student = Student.objects.get(pk=pk)
+
+        # return render(request, 'students/edit.html', {'form': form})
+    if request == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            print request.POST
+            form.save()
+            messages.success(request, 'Info on the student has been sucessfully changed.')
+    else:
+        form = StudentModelForm(instance=student)
+    return render(request, 'students/edit.html', {'form': form})
+
