602c3431ff388107ef37c90c16bd0a15c75ba9bf
green <green@debian>
2016-03-10 21:29:55 +0200

    KZ12.1#2

--- students/models.py
+++ students/models.py
@@ -5,7 +5,7 @@ from courses.models import Course
 class Student(models.Model):
     name = models.CharField(verbose_name=u'Name', max_length=250)
     surname = models.CharField(verbose_name=u'Surname', max_length=250)
-    date_of_birth = models.DateField(verbose_name=u'Birthday')
+    date_of_birth = models.DateField(verbose_name=u'Date of birth')
     email = models.EmailField(verbose_name=u'Email')
     phone = models.CharField(verbose_name=u'Phone', max_length=25)
     address = models.CharField(verbose_name=u'Address', max_length=250)
